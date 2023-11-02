57dad0022efd34eac8436af52d807891bea4885a
whyDoILoveThis <136662855+whyDoILoveThis@users.noreply.github.com>
2023-11-02 08:02:19 -0400

    EXERCISE: Building a Button Component

--- JS/main.js
+++ JS/main.js
@@ -21,8 +21,15 @@ toTopLink.appendChild(toTopIcon);
 
 
 
+let domReady = (cb) => {
+  document.readyState === 'interactive' || document.readyState === 'complete'
+  ?(cb)
+  : document.addEventListener('DOMContentLoaded', cb);
+};
 
-
+domReady(() => {
+  document.body.style.visibility = 'visible';
+})
 
 
 
